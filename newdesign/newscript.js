3f964743868437f6038e995f3627d8dc690eb10c
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-03-29 11:44:51 -0500

    added animate.css styles

---
+++ newdesign/newscript.js
@@ -0,0 +1,43 @@
+
+// Smooth scrolling with anchor links
+$("a[href*=\\#]").on("click", function(event){     
+    event.preventDefault();
+    $("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+});
+
+// Scroll into view animations
+$(document).ready(function() {
+    // Check if element is scrolled into view
+    function isScrolledIntoView(elem) {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+  
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+  
+      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+    // If element is scrolled into view, fade it in from left. If not in view, fade out left.
+    $(window).scroll(function() {
+        $(".jeff-img").each(function() {
+            if (isScrolledIntoView(this) === true) {
+            $(this).removeClass("fadeOutLeft").addClass("fadeInLeft");
+            } else {
+                $(this).removeClass("fadeInLeft").addClass("fadeOutLeft");
+            };
+        });
+    });
+
+    // If element is scrolled into view, fade it in. If not in view, fade out.
+    $(window).scroll(function() {
+        $(".aboutMe").each(function() {
+            if (isScrolledIntoView(this) === true) {
+                $(this).removeClass("fadeOut slower").addClass("fadeIn slower");
+            } else {
+                $(this).removeClass("fadeIn slower").addClass("fadeOut slower");
+            };
+        });
+    });
+});
+
+  
